7b68f578a9dd639425ee841b71bd9ce35ca24b92
ZhouHongBo <252747500@qq.com>
2018-05-15 22:41:56 +0900

    整理代码

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,25 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 public class EnemySpawner : MonoBehaviour
 {
-    float waveTimer = 0;
-    float rateTimer = 0;
+    [BoxGroup("产生敌人的速率", centerLabel: true)]
     //产生敌人的速率
     public float Rate;
+    [BoxGroup("敌人的预制体", centerLabel: true)]
+    [SuffixLabel("Prefab")]
     //敌人的预制体
     public GameObject EnemeyPrefab;
+    [BoxGroup("每一波的时间", centerLabel: true)]
     //每一波的时间
-    public float TimesofEachWave = 30;
+    public float TimesOfEachWave = 30;
+    [BoxGroup("每一波的已经产生的数量", centerLabel: true)]
     //每一波的已经产生的数量
     public int Count;
+
+    float waveTimer = 0;
+    float rateTimer = 0;
 
     void Update()
     {
 
         waveTimer += Time.deltaTime;
-        if (waveTimer < TimesofEachWave && Count != 10)
+        if (waveTimer < TimesOfEachWave && Count != 10)
         {
             rateTimer += Time.deltaTime;
             if (rateTimer > Rate)
--- Assets/Scripts/InterpolationPath.cs
+++ Assets/Scripts/InterpolationPath.cs
@@ -1,44 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 public class InterpolationPath : MonoBehaviour
 {
-    public Transform[] path;
-
-    public float speed;
-    public Transform target;
+    [BoxGroup("路径名称", centerLabel: true)]
+    public string PathName;
+    [BoxGroup("路径移动速度", centerLabel: true)]
+    public float Speed;
+    /// <summary>
+    /// 上一帧的路径位置
+    /// </summary>
+    Vector3 lastPoint;
+    /// <summary>
+    /// 上一帧的百分比
+    /// </summary>
+    float percentLast;
+    /// <summary>
+    /// 当前的百分比
+    /// </summary>
+    float percentNow;
     // Use this for initialization
     void Start()
     {
-        lastPoint = target.position;
+        lastPoint = transform.position;
     }
 
-    Vector3 lastPoint;
-    float percentLast;
-    float percentNow;
     // Update is called once per frame
     void Update()
     {
-        lastPoint = target.position;
+        PathMove();
+    }
+
+    /// <summary>
+    /// 路径匀速移动（百分比）
+    /// </summary>
+    void PathMove()
+    {
+        //路径
+        Vector3[] path = iTweenPath.GetPath(PathName);
+        lastPoint = transform.position;
         percentLast = percentNow;
         percentNow += 0.001f;
         float distance = Vector3.Distance(iTween.PointOnPath(path, percentNow), iTween.PointOnPath(path, percentLast));
-        float percentAmend = 0.001f * speed * Time.deltaTime / distance;
+        float percentAmend = 0.001f * Speed * Time.deltaTime / distance;
         percentNow = percentLast + percentAmend;
-        target.position = iTween.PointOnPath(path, percentNow);
-        Vector3 lookPos = iTween.PointOnPath(path, percentNow += 0.001f) - target.transform.position;
+        //移动到当前百分比的路径位置
+        transform.position = iTween.PointOnPath(path, percentNow);
+        //面朝下一帧的路径
+        Vector3 lookPos = iTween.PointOnPath(path, percentNow += 0.001f) - transform.position;
         lookPos.y = 0;
         Quaternion rotation = Quaternion.LookRotation(lookPos);
-        target.transform.rotation = Quaternion.Slerp(target.transform.rotation, rotation, Time.deltaTime * 30.0f);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 30.0f);
+        //如果已经走完，让当前百分比归零
         if (percentNow > 1f)
         {
             percentNow = 0;
         }
-    }
-
-    private void OnDrawGizmos()
-    {
-        iTween.DrawPathGizmos(path, Color.red);
     }
 }
